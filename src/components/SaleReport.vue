30159f1e4c222017002972e626d40b6c092ae916
ytmzz <ytmzz@icloud.com>
2020-06-12 16:48:30 +0800

    commit by ytmzz 2020/06/12 with add role

--- src/components/SaleReport.vue
+++ src/components/SaleReport.vue
@@ -8,12 +8,12 @@
                 <el-select v-model="hospitalId" placeholder="--所有分销商--"
                            @change="">
                     <el-option value="-1" label="所有分销商"></el-option>
-<!--                    <el-option-->
-<!--                            v-for="item in hospitalList"-->
-<!--                            :key="item.hospitalId"-->
-<!--                            :label="item.hospitalname"-->
-<!--                            :value="item.hospitalId">-->
-<!--                    </el-option>-->
+                    <!--                    <el-option-->
+                    <!--                            v-for="item in hospitalList"-->
+                    <!--                            :key="item.hospitalId"-->
+                    <!--                            :label="item.hospitalname"-->
+                    <!--                            :value="item.hospitalId">-->
+                    <!--                    </el-option>-->
                 </el-select>
             </el-col>
         </el-row>
@@ -51,42 +51,30 @@
         <br/>
 
         <el-row>
-            <el-col :span="2"><el-button size="small">导出报表</el-button></el-col>
-            <el-col :span="3" :offset="19">
-                <el-button-group>
-                    <el-popover
-                            placement="bottom"
-                            trigger="click">
-                        <el-button size="small" type="primary" slot="reference">日</el-button>
-                    </el-popover><el-popover
-                            placement="bottom"
-                            width="200"
-                            trigger="click">
-                        <el-date-picker
-                            v-model="value2"
-                            type="month"
-                            placeholder="选择月"
-                            size="mini"
-                            @change="selectMonth()">
-                        </el-date-picker>
-                        <el-button size="small" slot="reference">月</el-button>
-                    </el-popover><el-popover
-                            placement="bottom"
-                            width="200"
-                            trigger="click">
-                        <el-date-picker
-                            v-model="value3"
-                            type="year"
-                            placeholder="选择年"
-                            size="mini"
-                            @change="selectYear()">
-                        </el-date-picker>
-                        <el-button size="small" slot="reference">年</el-button>
-                    </el-popover>
-                </el-button-group>
+            <el-col :span="2">
+                <el-button size="small">导出报表</el-button>
+            </el-col>
+            <el-col :span="dateWidth" style="float:right">
+                <!-- 日选择器，选择到月份 -->
+                <el-button size="small" v-show="!month.isClick" @click="pressDay">日</el-button>
+                <el-date-picker v-show="month.isClick" v-model="month.value" @change="getMonth"
+                                type="month"
+                                size="small"
+                                :clearable="false"
+                                value-format="yyyy/MM">
+                </el-date-picker>
+                <!-- 月选择器，选择到年 -->
+                <el-button size="small" v-show="!year.isClick" @click="pressMonth">月</el-button>
+                <el-date-picker v-show="year.isClick" v-model="year.value" @change="getYear"
+                                type="year"
+                                size="small"
+                                :clearable="false">
+                </el-date-picker>
+                <!-- 选择按年显示 -->
+                <el-button size="small" @click="pressYear">年</el-button>
             </el-col>
         </el-row>
-        <br />
+        <br/>
         <el-row style="background-color: #e3f5ff; border-radius: 0px; padding: 15px 10px">
             <i class="el-icon-user-solid"></i>&nbsp;&nbsp;床位数（张）: {{total}}
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -94,25 +82,16 @@
         </el-row>
 
         <el-table :data="tableData" border>
-<!--            <el-table-column prop="date" label="id" width="40">-->
-<!--                <template slot-scope="scope">{{scope.$index + 1}}</template>-->
-<!--            </el-table-column>-->
-<!--            <el-table-column prop="ordersId" label="订单号" width="120"></el-table-column>-->
-<!--            <el-table-column prop="bed.department.hospital.hospitalname" label="所属医院"></el-table-column>-->
-<!--            <el-table-column prop="bed.department.departmentname" label="所属科室"></el-table-column>-->
-<!--            <el-table-column prop="bedId" label="床位id"></el-table-column>-->
-<!--            <el-table-column prop="totalpay" label="实际支付"></el-table-column>-->
-<!--            <el-table-column prop="customer.phonenumber" label="手机号"></el-table-column>-->
-<!--            <el-table-column prop="ordersStatus" label="订单状态"></el-table-column>-->
-<!--            <el-table-column prop="createTime" label="下单时间" width="150">-->
-<!--                <template slot-scope="scope">{{ scope.row.createTime | dateFormat }}</template>-->
-<!--            </el-table-column>-->
-<!--            <el-table-column label="操作" width="150">-->
-<!--                <template slot-scope="scope">-->
-<!--                    <el-button type="small info" @click="getDetail(scope.row)">详情</el-button>-->
-<!--                    <el-button type="small danger">删除</el-button>-->
-<!--                </template>-->
-<!--            </el-table-column>-->
+            <el-table-column prop="date" label="id" width="40">
+                <template slot-scope="scope">{{scope.$index + 1}}</template>
+            </el-table-column>
+            <el-table-column prop="date" label="日期"></el-table-column>
+            <el-table-column prop="" label="分销商"></el-table-column>
+            <el-table-column prop="" label="医院名称"></el-table-column>
+            <el-table-column prop="" label="可是名称"></el-table-column>
+            <el-table-column prop="" label="床位数（张）"></el-table-column>
+            <el-table-column prop="" label="租赁次数（张）"></el-table-column>
+            <el-table-column prop="" label="总租金（元）"></el-table-column>
         </el-table>
         <el-pagination
                 style="text-align: center"
@@ -122,7 +101,6 @@
                 :page-size="pageSize"
                 @current-change="changePage">
         </el-pagination>
-
     </el-main>
 </template>
 
@@ -131,29 +109,69 @@
         name: "SaleReport",
         data() {
             return {
+                // 分页
                 total: '',
                 pageSize: '',
+                // 筛选条件
                 departmentId: '',
-                ordersStatus: '',
                 hospitalId: '',
+                userinfoId: '',
                 tableData: '',
-                value: '',
-                value2: '',
-                value3: '',
+                // 年月日选择器相关
+                dateWidth: 4,
+                month: {
+                    isClick: false,
+                    value: ''
+                },
+                year: {
+                    isClick: false,
+                    value: ''
+                }
             }
         },
         methods: {
             changePage() {
 
             },
-            selectMonth() {
-                var a = new Date(this.value2).getFullYear();
-                var b = new Date(this.value2).getMonth();
-                console.log(a);
-                console.log(b);
+            dateInit() {
+                this.dateWidth = 4;
+                this.month.isClick = false;
+                this.month.value = '';
+                this.year.isClick = false;
+                this.year.value = '';
             },
-            selectYear() {
-
+            pressDay() {
+                this.dateInit();
+                this.month.isClick = true;
+                this.dateWidth = 8;
+            },
+            pressMonth() {
+                this.dateInit();
+                this.year.isClick = true;
+                this.dateWidth = 8;
+            },
+            pressYear() {
+                // 直接获取按年显示数据
+            },
+            getMonth() {
+                this.month.isClick = false;
+                this.dateWidth = 4;
+                // 根据选择月显示每日数据
+            },
+            getYear() {
+                this.year.isClick = false;
+                this.dateWidth = 4;
+                // 根据选择年显示每月数据
+            }
+        },
+        filters: {
+            dateFormat(dataStr) {
+                var dt = new Date(dataStr);
+                // yyyy-mm-dd
+                var y = dt.getFullYear();
+                var m = dt.getMonth() + 1;
+                var d = dt.getDate();
+                return y + "-" + m + "-" + d
             }
         }
     }
